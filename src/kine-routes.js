019475427c9412cb1e40c35b91f43e696fba1652
Augusto Ciuro <augustociuro@gmail.com>
2026-04-12 23:14:29 -0300

    Change database module from kine-db to kine-db-postgres

--- src/kine-routes.js
+++ src/kine-routes.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const db = require('./kine-db');
+const db = require('./kine-db-postgres');
 
 const uploadsDir = path.join(process.env.DATA_DIR || path.join(__dirname, '..'), 'uploads');
 if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
