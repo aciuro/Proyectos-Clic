1b5667264e69d234d98f1288ef7e8f6083207e99
Augusto Ciuro <augustociuro@gmail.com>
2026-04-25 01:20:13 -0300

    fix: exponer ruta directa de rutinas clinicas

--- kinesiologia/client/src/main.jsx
+++ kinesiologia/client/src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import KineApp from './kine/KineApp.jsx'
+import ClinicalRoutinePatientPage from './kine/ClinicalRoutinePatientPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/kine/paciente/:id/rutinas-clinicas" element={<ClinicalRoutinePatientPage />} />
         <Route path="/kine/*" element={<KineApp />} />
         <Route path="/" element={<Navigate to="/kine" replace />} />
         <Route path="/*" element={<App />} />
