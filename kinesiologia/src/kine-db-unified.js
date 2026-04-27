8cba4d2916a5e49f55a624c2671efb4e4188ade1
Augusto Ciuro <augustociuro@gmail.com>
2026-04-26 23:08:40 -0300

    Fix missing aparicion on motivos

--- kinesiologia/src/kine-db-unified.js
+++ kinesiologia/src/kine-db-unified.js
@@ -1,1 +1,17 @@
-module.exports = require('./kine-db');
+const db = require('./kine-db');
+
+function fixMotivo(data) {
+  const d = Object.assign({}, data || {});
+  d.sintoma = d.sintoma || d.lesion || '';
+  d.aparicion = d.aparicion || '';
+  return d;
+}
+
+module.exports = Object.assign({}, db, {
+  insertMotivo(data) {
+    return db.insertMotivo(fixMotivo(data));
+  },
+  updateMotivo(data) {
+    return db.updateMotivo(fixMotivo(data));
+  }
+});
